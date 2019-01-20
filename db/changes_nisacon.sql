71e84c21a85be16c32f2ec4c0e91a9d3a731bea2
Julian Jacobi <mail@julianjacobi.net>
2019-01-20 21:34:14 +0100

    fixed nisacon database migration

--- db/changes_nisacon.sql
+++ db/changes_nisacon.sql
@@ -4,6 +4,6 @@ ALTER TABLE `users_contact` ADD COLUMN `zip_code` VARCHAR(255) NOT NULL;
 ALTER TABLE `users_contact` ADD COLUMN `emergency_contact` VARCHAR(255) NOT NULL;
 ALTER TABLE `users_contact` ADD COLUMN `emergency_contact_phone` VARCHAR(255) NOT NULL;
 
-ALTER TABLE `users_personal_data` ADD COLUMN `date_of_birth` DATE NOT NULL;
+ALTER TABLE `users_personal_data` ADD COLUMN `date_of_birth` DATETIME NOT NULL;
 ALTER TABLE `users_personal_data` ADD COLUMN `allergies` VARCHAR(511) NOT NULL;
 ALTER TABLE `users_personal_data` ADD COLUMN `medicines` VARCHAR(511) NOT NULL;
